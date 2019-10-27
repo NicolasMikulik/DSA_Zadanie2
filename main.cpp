3e2706d03963cf778822a672c3fec9e519a1a205
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-10-27 17:44:22 +0100

    pt1

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 #include <stdlib.h>
 #define SIZE 1000
 
+struct Item{
+    int key;
+    struct Item *next;
+};
+
 int hashFunctionOne(int key){
     if(key < 1){
         key = key * (-1);
@@ -26,46 +31,68 @@ int compressFunctionTwo(int key, int size){
     return (a*hash + b) % N;
 }
 
-void insert(int *hashArray, int key) {
+void insert(struct Item **hashArray, int key) {
     //printf("Inserting %d\n",key);
     int hashIndex = compressFunctionTwo(key, SIZE);
-    while(hashArray[hashIndex] != -1) {
-        if(hashArray[hashIndex] == key){
-            printf("Key %d ALREADY PRESENT in hash table at index %d.\n",key, hashIndex);
-            return;
-        }
-        ++hashIndex;
-        hashIndex %= SIZE;
+    if(hashArray[hashIndex] == nullptr){
+        hashArray[hashIndex] = (struct Item *)malloc(sizeof(struct Item));
+        hashArray[hashIndex]->next = hashArray[hashIndex];
+        hashArray[hashIndex]->key = key;
+        printf("Inserted key %d into an empty linked list at index %d.\n", key, hashIndex);
+        return;
     }
-    hashArray[hashIndex] = key;
+    else{
+        struct Item *start = hashArray[hashIndex];
+        struct Item *traversing = hashArray[hashIndex];
+        while(traversing->next != start){
+            if(traversing->key == key){
+                printf("Key %d already present in hash table at index %d.\n", key, hashIndex);
+                return;
+            }
+            traversing = traversing->next;
+        }
+        struct Item *added = (struct Item *)malloc(sizeof(struct Item));
+        added->key = key;
+        added->next = start;
+        traversing->next = added;
+        printf("Inserted key %d into an existing linked list at index %d.\n", key, hashIndex);
+        return;
+    }
 }
 
 
-int *find(int *hashArray, int key) {
+struct Item *find(struct Item **hashArray, int key) {
     int hashIndex = compressFunctionTwo(key, SIZE);
-    while(hashArray[hashIndex] != -1) {
-        if(hashArray[hashIndex] == key){
-            printf("Key %d found in hash table on index %d.\n", key, hashIndex);
-            return &hashArray[hashIndex];}
-        ++hashIndex;
-        hashIndex %= SIZE;
+    if(hashArray[hashIndex] == nullptr){
+        printf("Key %d not present in hash table.\n",key);
+        return nullptr;
     }
-    printf("Key %d not present in hash table.\n",key);
-    return NULL;
+    if(hashArray[hashIndex]->key == key){
+        printf("Key %d found in hash table on index %d.\n", key, hashIndex);
+        return hashArray[hashIndex];
+    }
+    else{
+        struct Item *start = hashArray[hashIndex];
+        struct Item *traversing = hashArray[hashIndex]->next;
+        while(traversing->next != start){
+            if(traversing->key == key){
+                printf("Key %d found in hash table at index %d.\n", key, hashIndex);
+                return hashArray[hashIndex];
+            }
+            traversing = traversing->next;
+        }
+        printf("Key %d not present in hash table.\n",key);
+        return nullptr;
+    }
 }
 
 // ukazkovy test
 int main(void)
 {
     int index = 0, read[SIZE], numberSet[SIZE];
-    int *hashArray=(int*)malloc(SIZE*sizeof(int));
-    if(hashArray == NULL){
-        printf("Unsuccessful malloc.\n");
-        exit(1);
-    }
-    else
-        for(index=0;index<SIZE;index++)
-            hashArray[index]=-1;
+    struct Item *hashArray[SIZE]; // = (struct Item*)malloc(SIZE*sizeof(struct Item));
+    for(index=0;index<SIZE;index++)
+        hashArray[index] = nullptr;
     FILE *source = fopen("./one_thousand_Source.txt", "r");
     FILE *search = fopen("./one_thousand_Search.txt","r");
     if(source == nullptr){printf("Unsuccessful opening of source file.\n"); exit(1);}
