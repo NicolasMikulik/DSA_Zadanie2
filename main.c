ffc0f3ff9392265a0432fae6e44911453ec7ab86
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-10-31 17:55:57 +0100

    pt11 recording own hash table times

--- main.c
+++ main.c
@@ -1,236 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdbool.h>
-#include <math.h>
 
-//A Red-Black tree node structure
-struct node
-{
-    int data;
-    char color;
-    struct node *left, *right, *parent;
+// uloha5-1.c -- Nicolas Mikulík, 20.10.2019 20:47
+
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 1000
+
+struct Item{
+    int key;
+    struct Item *next;
 };
 
-
-// Left Rotation
-void LeftRotate(struct node **root,struct node *x)
-{
-    if (!x || !x->right)
-        return ;
-    //y stored pointer of right child of x
-    struct node *y = x->right;
-
-    //store y's left subtree's pointer as x's right child
-    x->right = y->left;
-
-    //update parent pointer of x's right
-    if (x->right != NULL)
-        x->right->parent = x;
-
-    //update y's parent pointer
-    y->parent = x->parent;
-
-    // if x's parent is null make y as root of tree
-    if (x->parent == NULL)
-        (*root) = y;
-
-        // store y at the place of x
-    else if (x == x->parent->left)
-        x->parent->left = y;
-    else    x->parent->right = y;
-
-    // make x as left child of y
-    y->left = x;
-
-    //update parent pointer of x
-    x->parent = y;
-}
-
-
-// Right Rotation (Similar to LeftRotate)
-void rightRotate(struct node **root,struct node *y)
-{
-    if (!y || !y->left)
-        return ;
-    struct node *x = y->left;
-    y->left = x->right;
-    if (x->right != NULL)
-        x->right->parent = y;
-    x->parent =y->parent;
-    if (x->parent == NULL)
-        (*root) = x;
-    else if (y == y->parent->left)
-        y->parent->left = x;
-    else y->parent->right = x;
-    x->right = y;
-    y->parent = x;
-}
-
-// Utility function to fixup the Red-Black tree after standard BST insertion
-void insertFixUp(struct node **root,struct node *z)
-{
-    // iterate until z is not the root and z's parent color is red
-    while (z != *root && z != (*root)->left && z != (*root)->right && z->parent->color == 'R')
-    {
-        struct node *y;
-
-        // Find uncle and store uncle in y
-        if (z->parent && z->parent->parent && z->parent == z->parent->parent->left)
-            y = z->parent->parent->right;
-        else
-            y = z->parent->parent->left;
-
-        // If uncle is RED, do following
-        // (i)  Change color of parent and uncle as BLACK
-        // (ii) Change color of grandparent as RED
-        // (iii) Move z to grandparent
-        if (!y)
-            z = z->parent->parent;
-        else if (y->color == 'R')
-        {
-            y->color = 'B';
-            z->parent->color = 'B';
-            z->parent->parent->color = 'R';
-            z = z->parent->parent;
-        }
-
-            // Uncle is BLACK, there are four cases (LL, LR, RL and RR)
-        else
-        {
-            // Left-Left (LL) case, do following
-            // (i)  Swap color of parent and grandparent
-            // (ii) Right Rotate Grandparent
-            if (z->parent == z->parent->parent->left &&
-                z == z->parent->left)
-            {
-                char ch = z->parent->color ;
-                z->parent->color = z->parent->parent->color;
-                z->parent->parent->color = ch;
-                rightRotate(root,z->parent->parent);
-            }
-
-            // Left-Right (LR) case, do following
-            // (i)  Swap color of current node  and grandparent
-            // (ii) Left Rotate Parent
-            // (iii) Right Rotate Grand Parent
-            if (z->parent && z->parent->parent && z->parent == z->parent->parent->left &&
-                z == z->parent->right)
-            {
-                char ch = z->color ;
-                z->color = z->parent->parent->color;
-                z->parent->parent->color = ch;
-                LeftRotate(root,z->parent);
-                rightRotate(root,z->parent->parent);
-            }
-
-            // Right-Right (RR) case, do following
-            // (i)  Swap color of parent and grandparent
-            // (ii) Left Rotate Grandparent
-            if (z->parent && z->parent->parent &&
-                z->parent == z->parent->parent->right &&
-                z == z->parent->right)
-            {
-                char ch = z->parent->color ;
-                z->parent->color = z->parent->parent->color;
-                z->parent->parent->color = ch;
-                LeftRotate(root,z->parent->parent);
-            }
-
-            // Right-Left (RL) case, do following
-            // (i)  Swap color of current node  and grandparent
-            // (ii) Right Rotate Parent
-            // (iii) Left Rotate Grand Parent
-            if (z->parent && z->parent->parent && z->parent == z->parent->parent->right &&
-                z == z->parent->left)
-            {
-                char ch = z->color ;
-                z->color = z->parent->parent->color;
-                z->parent->parent->color = ch;
-                rightRotate(root,z->parent);
-                LeftRotate(root,z->parent->parent);
-            }
-        }
+int hashFunctionOne(int key){
+    if(key < 1){
+        key = key * (-1);
     }
-    (*root)->color = 'B'; //keep root always black
+    int number = key, hash = 0;
+    while(number > 10){
+        hash = 31*hash + number%10;
+        number /= 10;
+    }
+    return hash;
 }
 
-// Utility function to insert newly node in RedBlack tree
-void insert(struct node **root, int data)
-{
-    // Allocate memory for new node
-    struct node *z = (struct node*)malloc(sizeof(struct node));
-    z->data = data;
-    z->left = z->right = z->parent = NULL;
+int compressFunctionTwo(int key){
+    int hash = hashFunctionOne(key);
+    int a = 24, b = 5, N = 1009;
+    return (a*hash + b) % N;
+}
 
-    //if root is null make z as root
-    if (*root == NULL)
-    {
-        z->color = 'B';
-        (*root) = z;
+void insert(struct Item **hashArray, int key) {
+    //printf("Inserting %d\n",key);
+    int hashIndex = compressFunctionTwo(key);
+    if(hashArray[hashIndex] == NULL){
+        hashArray[hashIndex] = (struct Item *)malloc(sizeof(struct Item));
+        hashArray[hashIndex]->next = hashArray[hashIndex];
+        hashArray[hashIndex]->key = key;
+        //printf("Inserted key %d into an empty linked list at index %d.\n", key, hashIndex);
+        return;
     }
-    else
-    {
-        struct node *y = NULL;
-        struct node *x = (*root);
-
-        // Follow standard BST insert steps to first insert the node
-        while (x != NULL)
-        {
-            if(x->data == z->data){
-                //printf("Not allowing duplicate values: %d\n",z->data);
+    else{
+        struct Item *start = hashArray[hashIndex];
+        struct Item *traversing = hashArray[hashIndex];
+        while(traversing->next != start){
+            if(traversing->key == key){
+                //printf("Key %d already present in own hash table at index %d.\n", key, hashIndex);
                 return;
             }
-            y = x;
-            if (z->data < x->data)
-                x = x->left;
-            else
-                x = x->right;
+            traversing = traversing->next;
         }
-        z->parent = y;
-        if (z->data > y->data)
-            y->right = z;
-        else
-            y->left = z;
-        z->color = 'R';
-
-        // call insertFixUp to fix reb-black tree's property if it
-        // is voilated due to insertion.
-        insertFixUp(root,z);
-    }
-}
-
-struct node* checkPresence(struct node* node, int key){
-    if(node == NULL || node->data == key){
-        return node;}
-    if(key < node->data){
-        return checkPresence(node->left, key);}
-    return checkPresence(node->right, key);
-}
-
-/*void inorderTraversal(struct node *root, int * array, int *index){
-    if(root == NULL)
+        struct Item *added = (struct Item *)malloc(sizeof(struct Item));
+        added->key = key;
+        added->next = start;
+        traversing->next = added;
+        //printf("Inserted key %d into an existing linked list at index %d.\n", key, hashIndex);
         return;
-    else{
-        if(root != NULL){
-            inorderTraversal(root->left, array, index);
-            array[*index] = root->data;
-            *index = *index + 1;
-            inorderTraversal(root->right, array, index);
-        }
     }
-    return;
-}*/
+}
+
+
+struct Item *find(struct Item **hashArray, int key) {
+    int hashIndex = compressFunctionTwo(key);
+    if(hashArray[hashIndex] == NULL){
+        //printf("Key %d not present in own hash table.\n",key);
+        return NULL;
+    }
+    if(hashArray[hashIndex]->key == key){
+        //printf("Key %d found in own hash table on index %d.\n", key, hashIndex);
+        return hashArray[hashIndex];
+    }
+    else{
+        struct Item *start = hashArray[hashIndex];
+        struct Item *traversing = hashArray[hashIndex]->next;
+        while(traversing->next != start){
+            if(traversing->key == key){
+                //printf("Key %d found in own hash table at index %d.\n", key, hashIndex);
+                return hashArray[hashIndex];
+            }
+            traversing = traversing->next;
+        }
+        //printf("Key %d not present in own hash table.\n",key);
+        return NULL;
+    }
+}
 
 // ukazkovy test
 int main(void)
 {
-    //unsigned int *array = (unsigned int *)malloc(100000*sizeof(unsigned int));
-    int index = 0, key = 0, numberSet[25000];
+    int index = 0, key = 0, numberSet[500];
+    struct Item *hashArray[SIZE];
+    for(index=0;index<SIZE;index++)
+        hashArray[index] = NULL;
     struct timespec tstart={0,0}, tend={0,0};
-    FILE *source = fopen("./fifty_thousand_Source.txt", "r");
-    FILE *searchInput = fopen("./fifty_thousand_Search.txt","r");
-    FILE *insertRecord = fopen("./fifty_thousand_RedBlack_times.txt", "w");
+    FILE *source = fopen("./one_thousand_Source.txt", "r");
+    FILE *searchInput = fopen("./one_thousand_Search.txt","r");
+    FILE *insertRecord = fopen("./one_thousand_ownHash_times.txt", "w");
     if(source == NULL){printf("Unsuccessful opening of source file.\n"); exit(1);}
     if(searchInput == NULL){printf("Unsuccessful opening of search file.\n"); exit(1);}
     if(insertRecord == NULL){printf("Unsuccessful opening of recording file.\n"); exit(1);}
@@ -239,18 +105,10 @@ int main(void)
     while(fscanf(source, "%d", &key) == 1){
         printf("%d\n", key);
         clock_gettime(CLOCK_MONOTONIC, &tstart);
-        if(NULL == checkPresence(root,key)){
-            insert(&root,key);
-            clock_gettime(CLOCK_MONOTONIC, &tend);
-            fprintf(insertRecord, "%.9f\n", ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
+        insert(hashArray,key);
+        clock_gettime(CLOCK_MONOTONIC, &tend);
+        fprintf(insertRecord, "%.9f\n", ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
                                             ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
-        }
-        else{
-            clock_gettime(CLOCK_MONOTONIC, &tend);
-            printf("Key %d already present in red-black tree.\n", key);
-            fprintf(insertRecord, "%.9f\n", ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
-                                            ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
-        }
     }
     /*index = 0;
     inorder(root);
@@ -261,12 +119,8 @@ int main(void)
         printf("%d ", numberSet[index]);
         index++;}
 
-    for (index = 0; index < 25000; index++){
-        if(NULL == checkPresence(root,numberSet[index]))
-            printf("Key %d is not present in red-black tree.\n",numberSet[index]);
-        else
-            printf("Key %d FOUND in red-black tree.\n",numberSet[index]);
-    }
+    for (index = 0; index < 500; index++){
+        find(hashArray, numberSet[index]);}
     fclose(source);
     fclose(searchInput);
     fclose(insertRecord);
