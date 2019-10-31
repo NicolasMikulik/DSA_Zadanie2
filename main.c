db422b5d4e15764e3400b6a9df0111cf2798f007
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-10-31 17:42:47 +0100

    pt10 recorded red-black tree times

--- main.c
+++ main.c
@@ -1,155 +1,253 @@
-// uloha3-1.c -- Nicolas Mikulík, 7.10.2019 08:16
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <time.h>
+#include <stdbool.h>
+#include <math.h>
 
-struct Node{
-    int key;
-    int height;
-    struct Node *left;
-    struct Node *right;
+//A Red-Black tree node structure
+struct node
+{
+    int data;
+    char color;
+    struct node *left, *right, *parent;
 };
 
-int max(int a, int b) {return (a>b ? a : b);}
-int min( int a,  int b) {return (a<b ? a : b);}
 
-int getHeight(struct Node* node){
-    if(node == NULL)
-        return 0;
+// Left Rotation
+void LeftRotate(struct node **root,struct node *x)
+{
+    if (!x || !x->right)
+        return ;
+    //y stored pointer of right child of x
+    struct node *y = x->right;
+
+    //store y's left subtree's pointer as x's right child
+    x->right = y->left;
+
+    //update parent pointer of x's right
+    if (x->right != NULL)
+        x->right->parent = x;
+
+    //update y's parent pointer
+    y->parent = x->parent;
+
+    // if x's parent is null make y as root of tree
+    if (x->parent == NULL)
+        (*root) = y;
+
+        // store y at the place of x
+    else if (x == x->parent->left)
+        x->parent->left = y;
+    else    x->parent->right = y;
+
+    // make x as left child of y
+    y->left = x;
+
+    //update parent pointer of x
+    x->parent = y;
+}
+
+
+// Right Rotation (Similar to LeftRotate)
+void rightRotate(struct node **root,struct node *y)
+{
+    if (!y || !y->left)
+        return ;
+    struct node *x = y->left;
+    y->left = x->right;
+    if (x->right != NULL)
+        x->right->parent = y;
+    x->parent =y->parent;
+    if (x->parent == NULL)
+        (*root) = x;
+    else if (y == y->parent->left)
+        y->parent->left = x;
+    else y->parent->right = x;
+    x->right = y;
+    y->parent = x;
+}
+
+// Utility function to fixup the Red-Black tree after standard BST insertion
+void insertFixUp(struct node **root,struct node *z)
+{
+    // iterate until z is not the root and z's parent color is red
+    while (z != *root && z != (*root)->left && z != (*root)->right && z->parent->color == 'R')
+    {
+        struct node *y;
+
+        // Find uncle and store uncle in y
+        if (z->parent && z->parent->parent && z->parent == z->parent->parent->left)
+            y = z->parent->parent->right;
+        else
+            y = z->parent->parent->left;
+
+        // If uncle is RED, do following
+        // (i)  Change color of parent and uncle as BLACK
+        // (ii) Change color of grandparent as RED
+        // (iii) Move z to grandparent
+        if (!y)
+            z = z->parent->parent;
+        else if (y->color == 'R')
+        {
+            y->color = 'B';
+            z->parent->color = 'B';
+            z->parent->parent->color = 'R';
+            z = z->parent->parent;
+        }
+
+            // Uncle is BLACK, there are four cases (LL, LR, RL and RR)
+        else
+        {
+            // Left-Left (LL) case, do following
+            // (i)  Swap color of parent and grandparent
+            // (ii) Right Rotate Grandparent
+            if (z->parent == z->parent->parent->left &&
+                z == z->parent->left)
+            {
+                char ch = z->parent->color ;
+                z->parent->color = z->parent->parent->color;
+                z->parent->parent->color = ch;
+                rightRotate(root,z->parent->parent);
+            }
+
+            // Left-Right (LR) case, do following
+            // (i)  Swap color of current node  and grandparent
+            // (ii) Left Rotate Parent
+            // (iii) Right Rotate Grand Parent
+            if (z->parent && z->parent->parent && z->parent == z->parent->parent->left &&
+                z == z->parent->right)
+            {
+                char ch = z->color ;
+                z->color = z->parent->parent->color;
+                z->parent->parent->color = ch;
+                LeftRotate(root,z->parent);
+                rightRotate(root,z->parent->parent);
+            }
+
+            // Right-Right (RR) case, do following
+            // (i)  Swap color of parent and grandparent
+            // (ii) Left Rotate Grandparent
+            if (z->parent && z->parent->parent &&
+                z->parent == z->parent->parent->right &&
+                z == z->parent->right)
+            {
+                char ch = z->parent->color ;
+                z->parent->color = z->parent->parent->color;
+                z->parent->parent->color = ch;
+                LeftRotate(root,z->parent->parent);
+            }
+
+            // Right-Left (RL) case, do following
+            // (i)  Swap color of current node  and grandparent
+            // (ii) Right Rotate Parent
+            // (iii) Left Rotate Grand Parent
+            if (z->parent && z->parent->parent && z->parent == z->parent->parent->right &&
+                z == z->parent->left)
+            {
+                char ch = z->color ;
+                z->color = z->parent->parent->color;
+                z->parent->parent->color = ch;
+                rightRotate(root,z->parent);
+                LeftRotate(root,z->parent->parent);
+            }
+        }
+    }
+    (*root)->color = 'B'; //keep root always black
+}
+
+// Utility function to insert newly node in RedBlack tree
+void insert(struct node **root, int data)
+{
+    // Allocate memory for new node
+    struct node *z = (struct node*)malloc(sizeof(struct node));
+    z->data = data;
+    z->left = z->right = z->parent = NULL;
+
+    //if root is null make z as root
+    if (*root == NULL)
+    {
+        z->color = 'B';
+        (*root) = z;
+    }
     else
-        return node->height;
-}
+    {
+        struct node *y = NULL;
+        struct node *x = (*root);
 
-int getBalance(struct Node* node){
-    if(node == NULL)
-        return 0;
-    return getHeight(node->right) - getHeight(node->left);
-}
-
-struct Node* newNode(int key){
-    struct Node *node=(struct Node*)malloc(sizeof(struct Node));
-    node->key=key;
-    node->left=NULL;
-    node->right=NULL;
-    node->height=1;
-    return node;
-}
-
-struct Node* rotateLeft(struct Node* pivot){
-    struct Node* pivotRightChiu = pivot->right;
-    struct Node* leftChiu = pivotRightChiu->left;
-
-    pivotRightChiu->left = pivot;
-    pivot->right = leftChiu;
-
-    pivot->height = 1 + max(pivot->left->height, pivot->right->height);
-    pivotRightChiu->height = 1 + max(pivotRightChiu->left->height, pivotRightChiu->right->height);
-    return pivot;
-}
-
-struct Node* rotateRight(struct Node* pivot){
-    struct Node* pivotLeftChiu = pivot->left;
-    struct Node* rightChiu = pivotLeftChiu->right;
-
-    pivotLeftChiu->right = pivot;
-    pivot->left = rightChiu;
-
-    pivot->height = 1 + max(pivot->left->height, pivot->right->height);
-    pivotLeftChiu->height = 1 + max(pivotLeftChiu->left->height, pivotLeftChiu->right->height);
-    return pivot;
-}
-
-struct Node* insert(struct Node* node, int key){
-    //printf("Height of root is %d.\n", getHeight);
-    if(node == NULL)
-        return newNode(key);
-    if(key < node->key){
-        //printf("INserting %d to the left of %d\n", key, node->key);
-        node->left = insert(node->left, key);}
-    else{
-        //printf("INserting %d to the RIGHT of %d\n", key, node->key);
-        node->right = insert(node->right, key);}
-    node->height =  + max(getHeight(node->left), getHeight(node->right));
-    int balance = getBalance(node);
-
-    if(balance < -1){ //left subtree is longer
-        if(node->left->key > key){ //left chiu
-            rotateRight(node);
+        // Follow standard BST insert steps to first insert the node
+        while (x != NULL)
+        {
+            if(x->data == z->data){
+                //printf("Not allowing duplicate values: %d\n",z->data);
+                return;
+            }
+            y = x;
+            if (z->data < x->data)
+                x = x->left;
+            else
+                x = x->right;
         }
-        else{ //right chiu
-            rotateLeft(node->left);
-            rotateRight(node);
-        }
+        z->parent = y;
+        if (z->data > y->data)
+            y->right = z;
+        else
+            y->left = z;
+        z->color = 'R';
+
+        // call insertFixUp to fix reb-black tree's property if it
+        // is voilated due to insertion.
+        insertFixUp(root,z);
     }
-    if(balance > 1){ //right subtree is longer
-        if(node->right->key < key){ //right chiu
-            rotateLeft(node);
-        }
-        else{ //left chiu
-            rotateRight(node->right);
-            rotateLeft(node);
-        }
-    }
-    return node;
 }
 
-struct Node* checkPresence(struct Node* node, int key){
-    if(node == NULL || node->key == key){
+struct node* checkPresence(struct node* node, int key){
+    if(node == NULL || node->data == key){
         return node;}
-    if(key < node->key){
+    if(key < node->data){
         return checkPresence(node->left, key);}
     return checkPresence(node->right, key);
 }
 
-void inorderTraversal(struct Node *root, int * array, int *index){
+/*void inorderTraversal(struct node *root, int * array, int *index){
     if(root == NULL)
         return;
     else{
         if(root != NULL){
             inorderTraversal(root->left, array, index);
-            array[*index] = root->key;
+            array[*index] = root->data;
             *index = *index + 1;
             inorderTraversal(root->right, array, index);
         }
     }
     return;
-}
+}*/
 
 // ukazkovy test
 int main(void)
 {
-    /*struct Node *root=newNode(277);
-    //root = insert(root,-117);
-    printf("root %d\n", root->key);
-    insert(root,386);
-    insert(root, -156);
-    insert(root, -117);
-    printf("root->left %d root %d root->right %d\n", root->left->key, root->key, root->left->right->key);*/
     //unsigned int *array = (unsigned int *)malloc(100000*sizeof(unsigned int));
     int index = 0, key = 0, numberSet[25000];
     struct timespec tstart={0,0}, tend={0,0};
     FILE *source = fopen("./fifty_thousand_Source.txt", "r");
     FILE *searchInput = fopen("./fifty_thousand_Search.txt","r");
-    FILE *insertRecord = fopen("./fifty_thousand_AVL_times.txt", "w");
+    FILE *insertRecord = fopen("./fifty_thousand_RedBlack_times.txt", "w");
     if(source == NULL){printf("Unsuccessful opening of source file.\n"); exit(1);}
     if(searchInput == NULL){printf("Unsuccessful opening of search file.\n"); exit(1);}
     if(insertRecord == NULL){printf("Unsuccessful opening of recording file.\n"); exit(1);}
-    fscanf(source, "%d", &key);
-    struct Node *root=newNode(key);
+    //fscanf(source, "%d", &key);
+    struct node *root=NULL;//newNode(key);
     while(fscanf(source, "%d", &key) == 1){
         printf("%d\n", key);
         clock_gettime(CLOCK_MONOTONIC, &tstart);
         if(NULL == checkPresence(root,key)){
-            insert(root,key);
+            insert(&root,key);
             clock_gettime(CLOCK_MONOTONIC, &tend);
             fprintf(insertRecord, "%.9f\n", ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
                                             ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
         }
         else{
             clock_gettime(CLOCK_MONOTONIC, &tend);
-            printf("Key %d already present in AVL tree.\n", key);
+            printf("Key %d already present in red-black tree.\n", key);
             fprintf(insertRecord, "%.9f\n", ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
                                             ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
         }
@@ -165,9 +263,9 @@ int main(void)
 
     for (index = 0; index < 25000; index++){
         if(NULL == checkPresence(root,numberSet[index]))
-            printf("Key %d is not present in AVL tree.\n",numberSet[index]);
+            printf("Key %d is not present in red-black tree.\n",numberSet[index]);
         else
-            printf("Key %d FOUND in AVL tree.\n",numberSet[index]);
+            printf("Key %d FOUND in red-black tree.\n",numberSet[index]);
     }
     fclose(source);
     fclose(searchInput);
