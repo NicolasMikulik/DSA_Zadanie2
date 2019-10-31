bbaf768b251ea4df5afdeb46260538f1fd216800
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-10-31 18:40:49 +0100

    pt13 recorded linear hash table times

--- main.c
+++ main.c
@@ -1,102 +1,111 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-
-// uloha5-1.c -- Nicolas Mikulík, 20.10.2019 20:47
-
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 #define SIZE 50000
 
-struct Item{
+struct DataItem {
+    int data;
     int key;
-    struct Item *next;
 };
 
-int hashFunctionOne(int key){
-    if(key < 1){
-        key = key * (-1);
-    }
-    int number = key, hash = 0;
-    while(number > 10){
-        hash = 31*hash + number%10;
-        number /= 10;
-    }
-    return hash;
+struct DataItem* hashArray[SIZE];
+struct DataItem* dummyItem;
+struct DataItem* item;
+
+int hashCode(int key) {
+    if(key < 0)
+        key *= -1;
+    return key % SIZE;
 }
 
-int compressFunctionTwo(int key){
-    int hash = hashFunctionOne(key);
-    int a = 24, b = 5, N = 49999;
-    return (a*hash + b) % N;
-}
-
-void insert(struct Item **hashArray, int key) {
-    //printf("Inserting %d\n",key);
-    int hashIndex = compressFunctionTwo(key);
-    if(hashArray[hashIndex] == NULL){
-        hashArray[hashIndex] = (struct Item *)malloc(sizeof(struct Item));
-        hashArray[hashIndex]->next = hashArray[hashIndex];
-        hashArray[hashIndex]->key = key;
-        //printf("Inserted key %d into an empty linked list at index %d.\n", key, hashIndex);
-        return;
-    }
-    else{
-        struct Item *start = hashArray[hashIndex];
-        struct Item *traversing = hashArray[hashIndex];
-        while(traversing->next != start){
-            if(traversing->key == key){
-                //printf("Key %d already present in own hash table at index %d.\n", key, hashIndex);
-                return;
+struct DataItem *search(int key) {
+    //get the hash
+    int hashIndex = hashCode(key);
+    long checkedValue = (long) &hashArray[hashIndex], first = 0;
+    //move in array until an empty
+    while(hashArray[hashIndex] != NULL) {
+        if(hashArray[hashIndex]->key == key)
+            return hashArray[hashIndex];
+        if((long) &hashArray[hashIndex] == checkedValue){
+            if(first == 0)
+                first++;
+            else {
+                //printf("%d is not present in hash table with linear probing.\n", key);
+                break;
             }
-            traversing = traversing->next;
         }
-        struct Item *added = (struct Item *)malloc(sizeof(struct Item));
-        added->key = key;
-        added->next = start;
-        traversing->next = added;
-        //printf("Inserted key %d into an existing linked list at index %d.\n", key, hashIndex);
-        return;
+        //go to next cell
+        ++hashIndex;
+        //wrap around the table
+        hashIndex %= SIZE;
     }
+    return NULL;
 }
 
+void insert(int key,int data) {
+    struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
+    item->data = data;
+    item->key = key;
+    //printf("Key %d Data %d \n", item->key, item->data);
+    //get the hash
+    int hashIndex = hashCode(key);
 
-struct Item *find(struct Item **hashArray, int key) {
-    int hashIndex = compressFunctionTwo(key);
-    if(hashArray[hashIndex] == NULL){
-        //printf("Key %d not present in own hash table.\n",key);
-        return NULL;
-    }
-    if(hashArray[hashIndex]->key == key){
-        //printf("Key %d found in own hash table on index %d.\n", key, hashIndex);
-        return hashArray[hashIndex];
-    }
-    else{
-        struct Item *start = hashArray[hashIndex];
-        struct Item *traversing = hashArray[hashIndex]->next;
-        while(traversing->next != start){
-            if(traversing->key == key){
-                //printf("Key %d found in own hash table at index %d.\n", key, hashIndex);
-                return hashArray[hashIndex];
-            }
-            traversing = traversing->next;
+    //move in array until an empty or deleted cell
+    while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1) {
+        if(hashArray[hashIndex]->key == key || hashArray[hashIndex]->data == data){
+            //printf("%d already present in hash table with linear probing.\n", hashArray[hashIndex]->data);
+            return;
         }
-        //printf("Key %d not present in own hash table.\n",key);
-        return NULL;
+        //go to next cell
+        ++hashIndex;
+        //wrap around the table
+        hashIndex %= SIZE;
     }
+    hashArray[hashIndex] = item;
+}
+
+struct DataItem* delete(struct DataItem* item) {
+    int key = item->key;
+    //get the hash
+    int hashIndex = hashCode(key);
+    //move in array until an empty
+    while(hashArray[hashIndex] != NULL) {
+        if(hashArray[hashIndex]->key == key) {
+            struct DataItem* temp = hashArray[hashIndex];
+            //assign a dummy item at deleted position
+            hashArray[hashIndex] = dummyItem;
+            return temp;
+        }
+        //go to next cell
+        ++hashIndex;
+        //wrap around the table
+        hashIndex %= SIZE;
+    }
+    return NULL;
+}
+
+void display() {
+    int i = 0;
+    for(i = 0; i<SIZE; i++) {
+        if(hashArray[i] != NULL)
+            printf(" (%d,%d)",hashArray[i]->key,hashArray[i]->data);
+        else
+            printf(" ~~ ");
+    }
+    printf("\n");
 }
 
 // ukazkovy test
 int main(void)
 {
     int index = 0, key = 0, numberSet[25000];
-    struct Item *hashArray[SIZE];
-    for(index=0;index<SIZE;index++)
-        hashArray[index] = NULL;
     struct timespec tstart={0,0}, tend={0,0};
     FILE *source = fopen("./fifty_thousand_Source.txt", "r");
     FILE *searchInput = fopen("./fifty_thousand_Search.txt","r");
-    FILE *insertRecord = fopen("./fifty_thousand_ownHash_times.txt", "w");
+    FILE *insertRecord = fopen("./fifty_thousand_linearHash_times.txt", "w");
     if(source == NULL){printf("Unsuccessful opening of source file.\n"); exit(1);}
     if(searchInput == NULL){printf("Unsuccessful opening of search file.\n"); exit(1);}
     if(insertRecord == NULL){printf("Unsuccessful opening of recording file.\n"); exit(1);}
@@ -105,7 +114,7 @@ int main(void)
     while(fscanf(source, "%d", &key) == 1){
         printf("%d\n", key);
         clock_gettime(CLOCK_MONOTONIC, &tstart);
-        insert(hashArray,key);
+        insert(key,key);
         clock_gettime(CLOCK_MONOTONIC, &tend);
         fprintf(insertRecord, "%.9f\n", ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
                                             ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
@@ -120,7 +129,11 @@ int main(void)
         index++;}
 
     for (index = 0; index < 25000; index++){
-        find(hashArray, numberSet[index]);}
+        if(search(numberSet[index]) == NULL)
+            printf("Key %d is not present in hash table with linear probing.\n", numberSet[index]);
+        else
+            printf("Key %d FOUND in hash table with linear probing.\n", numberSet[index]);
+    }
     fclose(source);
     fclose(searchInput);
     fclose(insertRecord);
